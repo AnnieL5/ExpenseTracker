af276af2b4696536b13a11bc9231b62c3ab8864b
AnnieL5 <alian5@ocdsb.ca>
2026-02-15 14:06:31 -0500

    update gridbaglayout

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.awt.*;
 import javax.swing.*;
 
-
 public class Main {
     public static topGUI topgui;
+
     public static void main(String[] args) throws Exception {
         // Creating instance of JFrame
         JFrame frame = new JFrame();
@@ -12,21 +12,21 @@ public class Main {
         // // Creating instance of JButton
         // JButton button = new JButton(" Add");
 
-        // // x axis, y axis, width, height
+        // x axis, y axis, width, height
         // button.setBounds(150, 200, 220, 50);
 
         // using no layout managers
-        // frame.setLayout(new BorderLayout());
+        frame.setLayout(new BorderLayout());
 
-        // topgui = new topGUI();
+        topgui = new topGUI();
 
-        // // adding button in JFrame
-        // frame.add(topgui, BorderLayout.PAGE_START);
+        // adding button in JFrame
+        frame.add(topgui, BorderLayout.PAGE_START);
 
-        // // 400 width and 500 height
-        // frame.setSize(500, 600);
+        // 400 width and 500 height
+        frame.setSize(500, 600);
 
-        frame.add(new GUI());
+        // frame.add(new GUI());
         // making the frame visible
         frame.setVisible(true);
     }
